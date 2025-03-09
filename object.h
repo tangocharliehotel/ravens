4dbaa82579b9e6a8b011634251462d24a71a1f70
TangoCharlieHotel <omegagit@pm.me>
2025-03-09 23:54:44 +0100

    Implemented calls and functions
    Implemented calls and functions. Also set up the support for native functions;

--- object.h
+++ object.h
@@ -2,17 +2,24 @@
 #define ravens_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)				(AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value)			isObjType(value, OBJ_FUNCTION)
+#define IS_NATIVE(value)			isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value)			isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value)			((ObjFunction*)AS_OBJ(value))
+#define AS_NATIVE(value)			(((ObjNative*)AS_OBJ(value))->function)
 //TODO: instead of two separate dynamic allocations, reaseach flexible array members
 #define AS_STRING(value)			((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)			(((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+	OBJ_FUNCTION,
+	OBJ_NATIVE,
 	OBJ_STRING,
 } ObjType;
 
@@ -21,6 +28,20 @@ struct Obj {
 	struct Obj* next;
 };
 
+typedef struct {
+	Obj obj;
+	int arity; //number of parameters function supports
+	Chunk chunk;
+	ObjString* name;
+} ObjFunction;
+
+typedef Value (*NativeFn)(int argCount, Value* args);
+
+typedef struct {
+	Obj obj;
+	NativeFn function;
+} ObjNative;
+
 struct ObjString {
 	Obj obj;
 	int length;
@@ -28,6 +49,8 @@ struct ObjString {
 	uint32_t hash;
 };
 
+ObjFunction* newFunction();
+ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
--- vm.h
+++ vm.h
@@ -2,16 +2,25 @@
 #define ravens_vm_h
 
 #include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
+#define FRAMES_MAX 64
 #define STACK_MAX 256
 
 //TODO: change so that errors go from fatal (and halt the interpertor), to just push error and enable user to recover form error
 
 typedef struct {
-	Chunk* chunk;
-	uint8_t* ip; //instruction pointer to keep track of instruction to be currently being executed by VM
+	ObjFunction* function;
+	uint8_t* ip;
+	Value* slots;
+} CallFrame;
+
+typedef struct {
+	CallFrame frames[FRAMES_MAX];
+	int frameCount; 
+	
 	Value stack[STACK_MAX];
 	Value* stackTop; //using direct pointer as it is faster to dereference poiter than calculate offset 
 					//from index each time. It always points to the last item (place for next item)
